2edbac7f7dd829394dcf380e993c7f3e18661d6d
emmanoelcardoos <emmanoel.cardoso09@gmail.com>
2026-08-20 21:16:20 -0300

    adicao do microsoft clarity

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script"; // ◄── 1. NOVA IMPORTAÇÃO AQUI
 import "./globals.css";
 
 const geistSans = localFont({
@@ -30,6 +31,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* ◄── 2. MICROSOFT CLARITY AQUI ──► */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "y5l0pw40z9");
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
